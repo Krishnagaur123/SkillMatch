dd32f1faaf746f7cc95e916cabaab39f24ceb043
Krishnagaur123 <krishnagaur227@gmail.com>
2026-08-02 12:12:39 +0530

    chore(s3): add AWS SDK infrastructure and configuration

---
+++ backend/src/main/java/com/skillmatch/config/S3Properties.java
@@ -0,0 +1,13 @@
+package com.skillmatch.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "app.s3")
+public class S3Properties {
+    private String bucket;
+    private String region;
+}
